e786cf0b760d9cae077b37b88e33f3e7f740f63f
Kingpin2557 <lode.snaet@student.howest.be>
2026-03-01 16:47:33 +0100

    Add logo SVG and Logo component; reorganize App navigation and tweak layout/styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import Logo from "./components/Icon/Logo";
 import { NavItem, NavMenu } from "./components/Navbar/nav";
 
 function App() {
   return (
     <>
-      <Button text="Free Zoom Call" />
-
-      <NavMenu>
-        <NavItem text="Home" url="./" isActive={true} />
-        <NavItem text="My Services" url="./" isActive={false} />
-        <NavItem text="Clients" url="./" isActive={false} />
-        <NavItem text="Resources" url="./" isActive={false} />
-      </NavMenu>
+      <nav>
+        <Logo />
+        <NavMenu>
+          <NavItem text="Home" url="./" isActive={true} />
+          <NavItem text="My Services" url="./" isActive={false} />
+          <NavItem text="Clients" url="./" isActive={false} />
+          <NavItem text="Resources" url="./" isActive={false} />
+        </NavMenu>
+        <Button text="Free Zoom Call" />
+      </nav>
     </>
   );
 }
